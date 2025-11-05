5e468d06745e875461ca37c792ab97bd92db499e
Cusoon77 <gopikrishnanpr234@gmail.com>
2025-11-05 15:29:58 +0530

    first commit

---
+++ merge.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], b[100];
+    int n1, n2, i, j, temp, total, size = 0;
+
+    printf("Enter how many elements in first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter the first array elements:\n");
+    for (i = 0; i < n1; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter how many elements in second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter the second array elements:\n");
+    for (i = 0; i < n2; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    total = n1 + n2;
+
+   
+    for (i = n1; i < total; i++) {
+        a[i] = b[size];
+        size++;
+    }
+
+    for (i = 0; i < total - 1; i++) {
+        for (j = 0; j < total - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted merged array elements are:\n");
+    for (i = 0; i < total; i++) {
+        printf("%d\n", a[i]);
+    }
+
+    return 0;
+}
+
