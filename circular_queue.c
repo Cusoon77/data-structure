ffd648cfd1db7825804fd5523692b96b2b91c247
Cusoon77 <143410239+Cusoon77@users.noreply.github.com>
2025-11-10 15:45:48 +0530

    second commit

---
+++ circular_queue.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+
+int queue[N];
+int rear = -1, front = -1;
+
+void enqueue() {
+    int val;
+    printf("Enter the data: ");
+    scanf("%d", &val);
+
+    if (rear == -1 && front == -1) { // Empty queue
+        rear = front = 0;
+        queue[rear] = val;
+    } else if ((rear + 1) % N == front) { // Queue full
+        printf("Queue is overflow\n");
+        return;
+    } else {
+        rear = (rear + 1) % N;
+        queue[rear] = val;
+    }
+}
+
+void dequeue() {
+    if (rear == -1 && front == -1) { // Empty queue
+        printf("Queue is underflow\n");
+        return;
+    } else if (rear == front) { // Only one element
+        printf("Deleted item is %d\n", queue[front]);
+        rear = front = -1;
+    } else {
+        printf("Deleted item is %d\n", queue[front]);
+        front = (front + 1) % N;
+    }
+}
+
+void search() {
+    int data, flag = 0, pos = 0;
+    if (rear == -1 && front == -1) {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    printf("Enter the data to search: ");
+    scanf("%d", &data);
+
+    int i = front;
+    while (1) {
+        if (queue[i] == data) {
+            flag = 1;
+            break;
+        }
+        if (i == rear) break;
+        i = (i + 1) % N;
+        pos++;
+    }
+
+    if (flag)
+        printf("The element is found at position %d\n", pos + 1);
+    else
+        printf("Element not found\n");
+}
+
+void display() {
+    if (rear == -1 && front == -1) {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    printf("Queue elements: ");
+    int i = front;
+    while (1) {
+        printf("%d ", queue[i]);
+        if (i == rear) break;
+        i = (i + 1) % N;
+    }
+    printf("\n");
+}
+
+int main() {
+    int opt;
+    do {
+        printf("\n1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Display\n");
+        printf("4. Search\n");
+        printf("5. Exit\n");
+        printf("Enter your option: ");
+        scanf("%d", &opt);
+
+        switch (opt) {
+            case 1: enqueue(); break;
+            case 2: dequeue(); break;
+            case 3: display(); break;
+            case 4: search(); break;
+            case 5: exit(0);
+            default: printf("Invalid option\n");
+        }
+    } while (1);
+
+    return 0;
+}
+
--- linkedlist.c
+++ linkedlist.c
@@ -1,175 +1,88 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-
-struct Node 
+#include<stdio.h>
+#include<stdlib.h>
+struct node 
 {
-    int data;
-    struct Node* next;
+int data;
+struct node*next;
 };
+struct node*newnode,*head=0;
 
-
-struct Node* head = NULL;
-void insertAtBeginning(int data) 
-{
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) 
-    {
-        printf("Memory allocation failed!\n");
-        return;
-    }
-    newNode->data = data;
-    newNode->next = head;
-    head = newNode;
-    printf("Inserted %d at the beginning.\n", data);
+void push() {
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("enter the data");
+scanf("%d",&newnode->data);
+newnode->next=head;
+head=newnode;
+printf("\n%d is pushed into the stack",head->data);
 }
-
-void insertAtEnd(int data) 
-{
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) 
-    {
-        printf("Memory allocation failed!\n");
-        return;
-    }
-    newNode->data = data;
-    newNode->next = NULL;
-
-    if (head == NULL)
-     {
-        head = newNode;
-        printf("Inserted %d (as the first node).\n", data);
-        return;
-    }
-
-    struct Node* current = head;
-    while (current->next != NULL) 
-    {
-        current = current->next;
-    }
-    current->next = newNode;
-    printf("Inserted %d at the end.\n", data);
+void pop() {
+struct node*temp;
+if(head==0)
+{printf("\nstack is empty ");
+return;
 }
-
-void deleteNode(int key)
- {
-    struct Node *current = head;
-    struct Node *prev = NULL;
-
- 
-    if (current != NULL && current->data == key) 
-    {
-        head = current->next;
-        free(current);
-        printf("Deleted %d (head node).\n", key);
-        return;
-    }
-    while (current != NULL && current->data != key)
-     {
-        prev = current;
-        current = current->next;
-    }
-
-    if (current == NULL) {
-        printf("Node with data %d not found.\n", key);
-        return;
-    }
-
-   
-    prev->next = current->next;
-    free(current);
-    printf("Deleted %d.\n", key);
-}
-
-
-void searchList(int key) 
+else
 {
-    struct Node* current = head;
-    int position = 1;
-
-    while (current != NULL)
-     {
-        if (current->data == key) 
+temp=head;
+printf("the delete data is %d",temp->data);
+head=head->next;
+free(temp);
+}
+}
+void peek()
+{
+if(head==0)
+{
+printf("\nstack is empty");
+return;
+}
+else
+printf("last element is %d" ,head->data);
+}
+void display()
+{
+struct node* temp;
+if(head==0)
+{
+printf("stack is empty");
+return;
+}
+else
+{
+temp=head;
+while(temp!=0)
+{
+printf("%d",temp->data);
+temp=temp->next;
+}
+} } 
+int main()
+{
+int opt;
+do
+{
+printf("\n1)PUSH\n");
+        printf("2)POP\n");
+        printf("3)PEEK\n");
+        printf("4)DISPLAY\n");
+        printf("5)QUIET");
+        printf("\nchoose your option : \n");
+        scanf("%d",&opt);
+        switch(opt)
         {
-            printf("Found %d at position %d.\n", key, position);
-            return;
-        }
-        current = current->next;
-        position++;
-    }
-    printf("%d not found in the list.\n", key);
-}
-
-void displayList() 
-{
-    struct Node* current = head;
-
-    if (current == NULL) 
-    {
-        printf("List is empty.\n");
-        return;
-}
-
-    printf("Current List: ");
-    while (current != NULL) 
-    {
-        printf("%d -> ", current->data);
-        current = current->next;
-    }
-    printf("NULL\n");
-}
-
-int main() {
-    int choice, data, key;
-
-    do {
-        printf("\n\n--- Singly Linked List Menu ---\n");
-        printf("1. Insert at Beginning\n");
-        printf("2. Insert at End\n");
-        printf("3. Delete Node (by value)\n");
-        printf("4. Search Node\n");
-        printf("5. Display List\n");
-        printf("6. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("Enter data: ");
-                scanf("%d", &data);
-                insertAtBeginning(data);
+          case 1:push();
                 break;
-            case 2:
-                printf("Enter data: ");
-                scanf("%d", &data);
-                insertAtEnd(data);
+          case 2:pop();
                 break;
-            case 3:
-                printf("Enter value to delete: ");
-                scanf("%d", &key);
-                deleteNode(key);
+          case 3:peek();
                 break;
-            case 4:
-                printf("Enter value to search: ");
-                scanf("%d", &key);
-                searchList(key);
+          case 4:display();
                 break;
-            case 5:
-                displayList();
-                break;
-            case 6:
-                printf("Exiting program. Clearing memory...\n");
-                while (head != NULL) 
-                {
-                    struct Node* temp = head;
-                    head = head->next;
-                    free(temp);
-                }
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
-        }
-    } while (choice != 6);
-
-    return 0;
-}
+          case 5:exit(0);
+          default: printf("invalid option\n");  
+      }
+    }while(1);
+  }
+          
+          
+          
